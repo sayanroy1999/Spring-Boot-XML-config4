cbb05f9a04300563419957c4118f2960cba03272
Spring Demo <sroy030499@gmail.com>
2024-04-16 12:14:53 +0530

    Initial commit

---
+++ src/main/java/com/example/demo4/Demo4Application.java
@@ -0,0 +1,21 @@
+package com.example.demo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class Demo4Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Demo4Application.class, args);
+
+		ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+		Person p= (Person)context.getBean("person");
+		System.out.println(p);
+
+	}
+
+}
